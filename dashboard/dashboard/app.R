d988c2a092f54837f3c5bd3e4bc04e0f691175c6
Timothy Deitz <psychlytx@gmail.com>
2019-10-05 22:54:19 +1000

    Make table joins and create value boxes.

--- dashboard/dashboard/app.R
+++ dashboard/dashboard/app.R
@@ -30,9 +30,41 @@ library(aws.s3)
 library(glue)
 
 
-library(shiny)
+pool <- dbPool( #Set up the connection with the db
+  drv = dbDriver("PostgreSQL"),
+  dbname = "scaladb",
+  host = "scaladb.cdanbvyi6gfm.ap-southeast-2.rds.amazonaws.com",
+  user = "jameslovie",
+  password = Sys.getenv("PGPASSWORD")
+)
 
-# Define UI for application that draws a histogram
+
+onStop(function() {
+  #Close pool object when session ends
+  poolClose(pool)
+
+})
+
+
+clinician_email<- "timothydeitz@gmail.com"  #Sys.getenv("SHINYPROXY_USERNAME")  ##This is how we will access the clinician username (i.e. email) to pass to the modules
+
+#url<- "https://scala.au.auth0.com/userinfo"
+
+#clinician_object<- httr::GET( url, httr::add_headers(Authorization = paste("Bearer", Sys.getenv("SHINYPROXY_OIDC_ACCESS_TOKEN")),
+#`Content-Type` = "application/json"))
+
+#clinician_object<- httr::content(clinician_object)
+
+clinician_id<- "auth0|5c99f47197d7ec57ff84527e" #paste(clinician_object["sub"]) #Access the id object
+
+
+data<- tbl(pool, "scale") %>% left_join(tbl(pool, "client"), by = c("client_id" = "id")) %>%
+  left_join(tbl(pool, "posttherapy_analytics"), by = c("client_id" = "client_id")) %>%
+  filter(clinician_id == clinician_id) %>% dplyr::collect()
+
+
+
+
 ui <- dashboardPage(
 
   dashboardHeader(
@@ -64,11 +96,12 @@ ui <- dashboardPage(
 
     fluidRow(
 
-      valueBoxOutput("improved"),
-      valueBoxOutput("sig_improved"),
-      valueBoxOutput("nil_change"),
-      valueBoxOutput("deteriorated")
+      valueBoxOutput("improved", width = 3),
+      valueBoxOutput("sig_improved", width = 3),
+      valueBoxOutput("remained_same", width = 3),
+      valueBoxOutput("deteriorated", width = 3)
     ),
+
 
     tabItems(
       # First tab content
@@ -101,7 +134,6 @@ ui <- dashboardPage(
 
                       )
       ),
-
 
 
       tabItem(tabName = "demographics",
@@ -155,6 +187,86 @@ ui <- dashboardPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+
+  #Join relevant tables and select variables relevant to dashboard
+
+  db_data<- tbl(pool, "scale") %>% inner_join(tbl(pool, "client"), by = c("client_id" = "id")) %>%
+    inner_join(tbl(pool, "posttherapy_analytics"), by = "client_id") %>% filter(clinician_id == clinician_id) %>%
+    dplyr::select(clinician_id, client_id, first_name, last_name, birth_date, sex, postcode, marital_status, sexuality,
+                  ethnicity, indigenous, children, workforce_status, education, entry_id, measure, subscale, date,
+                  score, pts, se, ci, ci_upper, ci_lower, contains("cutoff_value"), contains("cutoff_label"),
+                  principal_diagnosis, secondary_diagnosis, attendance_schedule, cancellations, non_attendances,
+                  attendances, premature_dropout, therapy, funding, private_health_fund, referrer, out_of_pocket) %>%
+    dplyr::collect()
+
+
+
+  #Create a list column with scores on a specific subscale for each client
+
+  nested<- db_data %>% dplyr::group_by(client_id, measure, subscale) %>% tidyr::nest()
+
+  #Arrange the date column within each nested dataframe, from earliest to latest
+
+  nested$data<- purrr::map(nested$data, ~dplyr::arrange(., lubridate::dmy(.x$date)))
+
+  #Within each nested dataframe, canculate key stats across all timepoints for that subscale
+
+  nested<- nested %>% dplyr::mutate(data = purrr::map(data, ~ dplyr::mutate(., improve_all = .x$score < lag( .x$score ),
+                                                                            sig_improve_all = .x$score < lag( .x$ci_lower ),
+                                                                            remained_same_all = .x$score == lag( .x$score ),
+                                                                            deteriorated_all = .x$score > lag( .x$score )
+  )))
+
+  #Use the nested dataframes to add new variables of key stats between first and last timepoint for each client per subscale
+
+  nested<- nested %>% dplyr::mutate(change = purrr::map_dbl( data, ~.x$score[1] - .x$score[length(.x$score)] ),
+                                    improve = purrr::map_lgl( data, ~ .x$score[length(.x$score)] < .x$score[1] ),
+                                    sig_improve = purrr::map_lgl( data, ~.x$score[length(.x$score)] < .x$ci_lower[1] ),
+                                    remained_same = purrr::map_lgl( data, ~ .x$score[length(.x$score)] == .x$score[1] ),
+                                    deteriorated = purrr::map_lgl(data, ~.x$score[length(.x$score)] > .x$score[1] )
+  )
+
+
+
+  total_clients_improved<- sum(nested$improve, na.rm = TRUE)
+
+  total_clients_sig_improved<- sum(nested$sig_improve, na.rm = TRUE)
+
+  total_clients_remained_same<- sum(nested$remained_same, na.rm = TRUE)
+
+  total_clients_deteriorated<- sum(nested$deteriorated, na.rm = TRUE)
+
+  total_administrations<- db_data %>% dplyr::select(entry_id) %>% dplyr::n_distinct()
+
+
+  output$improved <- renderValueBox({
+    valueBox(
+      value = paste0(total_clients_improved, " ", "(", (total_clients_improved/total_administrations) * 100, "%", ")"),
+      subtitle = "Clients Improved"
+    )
+  })
+
+  output$sig_improved <- renderValueBox({
+    valueBox(
+      value = paste0(total_clients_sig_improved, " ", "(", (total_clients_sig_improved/total_administrations) * 100, "%", ")"),
+      subtitle = "Clients Sigificantly Improved"
+    )
+  })
+
+  output$remained_same <- renderValueBox({
+    valueBox(
+      value = paste0(total_clients_remained_same, " ", "(", (total_clients_remained_same/total_administrations) * 100, "%", ")"),
+      subtitle = "Clients Did Not Change"
+    )
+  })
+
+  output$deteriorated <- renderValueBox({
+    valueBox(
+      value = paste0(total_clients_deteriorated, " ", "(", (total_clients_deteriorated/total_administrations) * 100, "%", ")"),
+      subtitle = "Clients Deteriorated"
+    )
+  })
+
 
 
 }
